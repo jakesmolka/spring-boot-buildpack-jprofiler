0d618d09e05a4002094714be6c509459fca96ad6
Jake Smolka <30796382+jakesmolka@users.noreply.github.com>
2024-10-24 09:33:44 +0200

    init

---
+++ src/main/java/org/example/springbootbuildpackjprofiler/SpringBootBuildpackJprofilerApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootbuildpackjprofiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBuildpackJprofilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBuildpackJprofilerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/springbootbuildpackjprofiler/SpringBootBuildpackJprofilerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootbuildpackjprofiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBuildpackJprofilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
